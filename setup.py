33b9c3446377f5729403dde743ffb245e74adcd4
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-11-30 17:58:41 +0000

    :heavy_plus_sign: Tests require mock

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ plugin_url = "https://github.com/cp2004/OctoPrint-EEprom-Marlin"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["mock"]
 
 # # --------------------------------------------------------------------------------------------------------------------
 # # More advanced options that you usually shouldn't have to touch follow after this point
